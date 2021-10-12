4363d8fd3e1114dc5ed1be68250d1e55f13877bc
fesavali <felixnzioki99@gmail.com>
2021-10-12 16:24:27 +0300

    import class R

--- app/src/main/java/com/savalicodes/globochat/MainActivity.kt
+++ app/src/main/java/com/savalicodes/globochat/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.savalicodes.globochat.R
 
 
 class MainActivity : AppCompatActivity() {
