2a90b8ff6862b6f865f7080bcc655bb563118022
fesavali <felixnzioki99@gmail.com>
2021-10-14 16:03:30 +0300

    update action on preference object

--- app/src/main/java/com/savalicodes/globochat/SettingsFragment.kt
+++ app/src/main/java/com/savalicodes/globochat/SettingsFragment.kt
@@ -6,10 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.sriyank.globochat.ChatListFragmentDirections
 import com.sriyank.globochat.SettingsFragmentDirections
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        TODO("Not yet implemented")
+        setPreferencesFromResource(R.xml.settings, rootKey)
 
+        val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
 
+        accSettingsPref?.setOnPreferenceClickListener {
+            val navHostFragment =
+                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navController = navHostFragment.navController
+            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+            navController.navigate(action)
+            true
+        }
+    }
 }
